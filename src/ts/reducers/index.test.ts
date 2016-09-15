fa2e933dc25da0be71660c6c0c36d6e2f8443003
Connormiha <connormiha1@gmail.com>
2016-09-16 01:05:54 +0300

    :arrow_up:

--- src/ts/reducers/index.test.ts
+++
@@ -1,9 +0,0 @@
-import reducer from './index';
-
-describe('Root reducer', () => {
-    it('Should return valid state', () => {
-        const result = reducer({}, {});
-
-        expect(result.people).toBeTruthy();
-    });
-});
